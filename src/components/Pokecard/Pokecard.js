9b4ee03ca4b9d75a05507e2c8d1bd5e72d9b3263
Alejandro Galaz <alejandrogalaz21@gmail.com>
2019-07-09 20:06:37 -0700

    comment fetch poke image

--- src/components/Pokecard/Pokecard.js
+++ src/components/Pokecard/Pokecard.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './pokecard.css'
 
 // const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
-const padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number)
+// const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+// const padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number)
 
-const Pokecard = ({ id, name, type, exp, src }) => {
-  let imgSrc = `${POKE_API}${padToThree(id)}.png`
+const Pokecard = ({ name, type, exp, src }) => {
+
   return (
     <div className="pokecard">
       <h1 className="pokecard-title">{name}</h1>
